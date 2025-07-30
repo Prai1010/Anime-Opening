0b834878f9a11910482b3a074d64ee1910fecd21
Anish Thapa <94629230+SaffronSan@users.noreply.github.com>
2025-07-30 13:12:20 -0400

    Added BASIC

---
+++ script.js
@@ -0,0 +1,36 @@
+let animeData = {
+}, userInput = {
+    current: 0,
+    currentRaw : "",
+    points : 0
+}
+function getData() {
+    fetch("./data.json")
+        .then((res) => res.json())
+        .then((data) => {
+            animeData.data = data;
+            localStorage.setItem("data", JSON.stringify(animeData));
+        })
+}
+function updateDom() {
+    $("#currentQues").text(userInput.current + 1)
+    $(".choiceBtn").each((ind,ele)=>{
+        $(ele).text(animeData.data[userInput.current].choices[ind])
+    })
+    $("audio").attr("src","audio/"+animeData.data[userInput.current].file)
+
+}
+$(document).ready(() => {
+    if (localStorage.getItem("data") === null) {
+        getData();
+        localStorage.setItem("user",JSON.stringify(userInput));
+    } else {
+        animeData = JSON.parse(localStorage.getItem("data"));
+        userInput = JSON.parse(localStorage.getItem("user"))
+    }
+    updateDom()
+    $("#startBtn").click((e) => {
+        $("#gameMenu").addClass("hidden")
+        $("#gameArea").removeClass("hidden")
+    })
+})
